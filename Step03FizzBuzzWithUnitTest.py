3fef2a6336ba612a2ae2ea0b2b3bc346f8d585fe
mathieu cans <mathieu.cans@orange.com>
2025-03-31 17:43:41 +0200

    Example of abstract test class for unittest

--- Step03FizzBuzzWithUnitTest.py
+++ Step03FizzBuzzWithUnitTest.py
@@ -1,20 +1,34 @@
 import unittest
+from abc import abstractmethod
+from unittest import TestCase
+
+from parameterized import parameterized
+
+from FirstFizzBuzz import FirstFizzBuzz
 
 
-def fizzbuzz(param):
-    if param == 3:
-        return "fizz"
-    return "{}".format(param)
+class FizzBuzzContractUnit:
+    @parameterized.expand([
+        ["say 1 for 1 ", 1, "1"],
+        ["say 2 for 2 ", 2, "2"],
+        ["say fizz for 3 ", 3, "fizz"],
+        ["say 4 for 4 ", 4, "4"],
+        ["say buzz for 5 ", 5, "buzz"],
+        ["say fizz for 6 ", 6, "fizz"],
+        ["say fizz for 9 ", 9, "fizz"],
+        ["say buzz for 10", 10, "buzz"],
+    ])
+    def test_fizz_buzz(self, name, a, expected):
+        self.assertEqual(expected, self.create().say(a))
+
+    @abstractmethod
+    def create(self):
+        """return the FizzBuzz implementation"""
 
 
-class StepO3FizzBuzzWithUnitTest(unittest.TestCase):
-    def test_say_1_for_1(self):
-        self.assertEqual( "1", fizzbuzz(1))
-    def test_say_2_for_2(self):
-        self.assertEqual( "2", fizzbuzz(2))
-    def test_say_fizz_for_3(self):
-        self.assertEqual( "fizz", fizzbuzz(3))
-
+class StepO3FizzBuzzWithUnitTest(FizzBuzzContractUnit, TestCase):
+    def create(self):
+        return FirstFizzBuzz()
 
 
 if __name__ == '__main__':
