64356a01eb9b9e29e02dec250f904ae751821b6d
mathieu cans <mathieu.cans@orange.com>
2025-03-31 12:03:08 +0200

    Fizz and buzz completed

--- Step04FizzBuzzWithPyTest.py
+++ Step04FizzBuzzWithPyTest.py
@@ -1,18 +1,24 @@
 import pytest
 
 def fizzbuzz(param):
-    if param == 3:
+    if param % 5 == 0:
+        return "buzz"
+    if param % 3 == 0:
         return "fizz"
     return "{}".format(param)
 
 class TestFizzBuzz:
-    def test_one_for_one(self):
-        assert fizzbuzz(1) == "1"
-
-    def test_two_for_two(self):
-        assert fizzbuzz(2) == "2"
-
-    def test_fizz_for_three(self):
-        assert fizzbuzz(3) == "fizz"
+    @pytest.mark.parametrize("input, expected", [
+        (1, "1"),
+        (2, "2"),
+        (3, "fizz"),
+        (4, "4"),
+        (5, "buzz"),
+        (6, "fizz"),
+        (9, "fizz"),
+        (10, "buzz"),
+    ])
+    def test_fizz_buzz(self, input, expected):
+        assert fizzbuzz(input) == expected
 
 
